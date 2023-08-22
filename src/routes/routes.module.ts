7294b8c633424217a8c2ce4a2f8a7f5a2c500525
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-08-22 16:23:08 +0000

    🎉 init(routes-driver): create routes-drive

--- src/routes/routes.module.ts
+++ src/routes/routes.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
 import { MapsModule } from '../maps/maps.module';
+import { RoutesDriverService } from './routes-driver/routes-driver.service';
 
 @Module({
   controllers: [RoutesController],
-  providers: [RoutesService],
+  providers: [RoutesService, RoutesDriverService],
   imports: [MapsModule]
 })
 export class RoutesModule { }
